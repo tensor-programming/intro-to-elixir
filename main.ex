962ba753d0e31ff8f972b23e09db5909a9608e1d
Tensor-Programming <abeltensor@tensor-programming.com>
2019-05-06 21:29:54 -0400

    Add files via upload

--- main.ex
+++ main.ex
@@ -1,164 +1,100 @@
-## Run Code in IEX (Interactive Elixir)
+defmodule Playground do
+  def extract_user(user) do
+    with {:ok, login} <- extract_login(user),
+         {:ok, email} <- extract_email(user),
+         {:ok, password} <- extract_password(user) do
+      {:ok, %{login: login, email: email, password: password}}
+    end
+  end
 
-## Numbers - Integers and Floats
-
-2 + 3
-3 * 4
-3 - 5
-x = 10
-## 5.0
-x / 2
-
-# 255
-0xFF
-
-3.14159
-
-3.0e-2
-
-div(5, 2)
-rem(5, 2)
-
-99_999_999_999_999_999_999_999_999_999_999_999_999_999_123_871_293_871_902_387_102 +
-  99_999_999_999_999_999_999_999_999_999_999_999_999_999_123_871_293_871_902_387_102
-
-## 1000000
-1_000_000
-
-## Atoms
-
-:atoms
-
-:"an atom"
-
-:"12387 &*^#2"
-
-var = :atom
-
-## Aliases
-
-AModule
-# true
-:"Elixir.AModule" == AModlue
-
-alias IO, as: MyIO
-
-MyIO.puts("Hello" == IO.puts("Hello"))
-
-## Booleans and nil
-
-true
-false
-true
-false
-
-# true
-true == true
-# true
-false == false
-
-true and false
-true or false
-not true
-
-nil
-nil
-nil == nil
-
-if 10 do
-  IO.puts("true")
+  defp extract_login(%{"login" => login}), do: {:ok, login}
+  defp extract_login(_), do: {:error, "login missing"}
+  defp extract_email(%{"email" => email}), do: {:ok, email}
+  defp extract_email(_), do: {:error, "email missing"}
+  defp extract_password(%{"password" => password}), do: {:ok, password}
+  defp extract_password(_), do: {:error, "password missing"}
 end
 
-# prints true
+# def extract_user(user) do
+#   case extract_login(user) do
+#     {:error, reason} ->
+#       {:error, reason}
 
-if nil do
-  IO.puts("true")
-end
+#     {:ok, login} ->
+#       case extract_email(user) do
+#         {:error, reason} ->
+#           {:error, reason}
 
-# get back nil
+#         {:ok, email} ->
+#           case extract_password(user) do
+#             {:error, reason} ->
+#               {:error, reason}
 
-# short circuit
-nil || false || 4 || true
-true && 5
-!5
-nil && 5
-true && 5 && nil && false
+#             {:ok, password} ->
+#               %{login: login, email: email, password: password}
+#           end
+#       end
+#   end
+# end
 
-## Tuples
+# defmodule Playground do
+#   # def max(a, b) do
+#   #   if a >= b, do: a, else: b
+#   # end
 
-cat = {"Jazzpurr", 12}
-age = elem(cat, 1)
-put_elem(cat, 1, 13)
-cat = put_elem(cat, 1, 23)
+#   # def max(a, b) do
+#   #   unless a >= b, do: b, else: a
+#   # end
 
-## Lists
+#   # def max(a, b) do
+#   #   cond do
+#   #     a >= b -> a
+#   #     true -> b
+#   #   end
+#   # end
 
-numbers = [1, 2, 3, 4]
-length(numbers)
-Enum.at(numbers, 5)
-Enum.at(numbers, 3)
-3 in numbers
-5 in numbers
+#   # def max(a, b) when a >= b do
+#   #   a
+#   # end
 
-List.replace_at(numbers, 0, 12)
-# [12, 2, 3, 4]
-List.insert_at(numbers, 3, 13)
-List.insert_at(numbers, -1, 13)
+#   # def max(a, b) do
+#   #   b
+#   # end
 
-[1, 2, 3] ++ [4, 5]
-[head | tail] = [1, 2, 3, 4, 5, 6, 7]
-# head = 1
-# tail = [2, 3, 4, 5, 6, 7]
+#   # def max(a, b) do
+#   #   case a >= b do
+#   #     true -> a
+#   #     false -> b
+#   #     _ -> :error
+#   #   end
+#   # end
 
-[1 | [2 | [3 | [4 | [5 | [6 | [7 | []]]]]]]]
+# end
 
-hd(numbers)
-tl(numbers)
+# defmodule Playground do
+#   def test(x) when is_number(x) and x < 0 do
+#     :negative
+#   end
 
-alist = [15, :atom, true]
-[:first | atlist]
-# [:first, 15, :atom, true]
+#   def test(0), do: :zero
 
-## Maps
+#   def test(x) when is_number(x) and x > 0 do
+#     :positive
+#   end
+# end
 
-empty = %{}
-sqrs = %{1 => 1, 2 => 4, 3 => 9}
+# defmodule Playground do
+#   def smallest(list) when length(list) > 0 do
+#     Enum.min(list)
+#   end
 
-sqrs[1]
-sqrs[2]
+#   def smallest(_), do: {:error, :invalid_argument}
+# end
 
-Map.get(sqrs, 3)
-Map.get(sqrs, 5)
+### number < atom < reference < function < port < pid
+### < tuple < map < list < bitstring (binary)
 
-cat = %{name: "Jazzpurr", age: 12}
-cat.age
-cat.name
-cat[:name]
-cat[:age]
-
-older_cat = %{cat | age: 15}
-older_cat.age
-
-## Binaries and Bitstrings
-<<1, 2, 3>>
-# <<0>>
-<<256>>
-# <<1>>
-<<257>>
-<<256::16>>
-<<1::4, 15::4>>
-<<1, 2>> <> <<3, 4>>
-
-## Strings and Charlist
-"This is a String"
-"Add together #{3 + 4}"
-"\r \n \" \\"
-
-~s("This is a string!")
-~S(This is a String!)
-
-'ABC' == [65, 66, 67]
-'sum: #{3 + 5}'
-~c(Character Sigil)
-# 'A String'
-String.to_charlist("A String")
+## (==, !=, ===, !==, >, <, <=, >=)
+## and, or, not, !
+## (+, -, *, /)
+## Kernel.is_number(term) Kernel.is_atom(term) etc
